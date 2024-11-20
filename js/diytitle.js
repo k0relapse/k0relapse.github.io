b02e9cd1d1e9318aa859293b73e72de62575abe2
k0relapse <k0relapse@outlook.com>
2024-11-20 21:10:59 +0800

    Site updated: 2024-11-20 21:10:57

--- js/diytitle.js
+++ js/diytitle.js
@@ -1,1 +1,14 @@
-var titleTime,OriginTitile=document.title;document.addEventListener("visibilitychange",function(){document.hidden?(document.title=" ₍˄·͈༝·͈˄*₎◞ ̑̑人走了就变回猫猫⑧~~",clearTimeout(titleTime)):(document.title="w(ﾟДﾟ)w你你你什么时候回来的?!"+OriginTitile,titleTime=setTimeout(function(){document.title=OriginTitile},2e3))});
+var OriginTitile = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        document.title = ' ₍˄·͈༝·͈˄*₎◞ ̑̑人走了就变回猫猫⑧~~';
+        clearTimeout(titleTime);
+    }
+    else {
+        document.title = 'w(ﾟДﾟ)w你你你什么时候回来的?!' + OriginTitile;
+        titleTime = setTimeout(function () {
+            document.title = OriginTitile;
+        }, 2000);
+    }
+});
