813128044295386305e641eb2f5a29124242ec76
k0relapse <k0relapse@outlook.com>
2025-03-05 23:25:50 +0800

    Site updated: 2025-03-05 23:25:49

--- js/randomTopImg.js
+++ js/randomTopImg.js
@@ -5,11 +5,13 @@ function randomPic() {
             'https://k0re.xyz/img/86.webp',
             'https://k0re.xyz/img/MisakuraRin.webp',
             'https://k0re.xyz/img/Misuzu.webp',
-            'https://k0re.xyz/img/Aisa.webp'
+            'https://k0re.xyz/img/Aisa.webp',
+            'https://k0re.xyz/img/Mizuna.webp',
+            'https://k0re.xyz/img/Rinchan.webp',
+            'https://k0re.xyz/img/Konron.webp'
         ]
         var url = background_urls[Math.floor((Math.random() * background_urls.length))];
         var urlPhoto = ('background-image:url(' + url + ')');
-        console.log("BackGround URL:" + url);
         backPicture.setAttribute("style", urlPhoto);
     }
 }
